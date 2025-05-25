182e416d014e4b0eb30533c1e54b2b8f6cf3111e
paolo-gajo <paolo.gajo2@unibo.it>
2025-05-25 04:16:43 +0000

    fix gat_experiments

--- gat_experiments.sh
+++ gat_experiments.sh
@@ -4,20 +4,23 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --time=06:00:00
-#SBATCH --output=./.slurm/%j_output.log
-#SBATCH --error=./.slurm/%j_error.log
+#SBATCH --output=./.slurm/%A/%a_output.log
+#SBATCH --error=./.slurm/%A/%a_error.log
 #SBATCH --mem=64g
-mkdir -p .slurm
+#SBATCH --array=0-N%999
+
+mkdir -p "./.slurm/$SLURM_ARRAY_JOB_ID"
+echo "Creating directory: ./.slurm/$SLURM_ARRAY_JOB_ID"
 nvidia-smi
 module load rust gcc arrow
 . .env/bin/activate
 
 declare -a use_gnn_steps_opts=(
-    1000
+    # 1000
     -1
     )
 declare -a gnn_enc_layers_opts=(
-    3
+    # 3
     0
     )
 declare -a parser_type_opts=(
@@ -27,6 +30,7 @@ declare -a parser_type_opts=(
     )
 top_k=1
 
+declare -a commands=()
 for use_gnn_steps in "${use_gnn_steps_opts[@]}"
 do
     for gnn_enc_layers in "${gnn_enc_layers_opts[@]}"
@@ -46,9 +50,20 @@ do
                         --parser_rnn_hidden_size 400
                         --parser_rnn_layers 0
                         --top_k $top_k"
-            echo $cmd
-            # $cmd
+            commands+=("$cmd")
         done
     done
 done
 
+total_combinations=${#commands[@]}
+
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+    command_to_run="${commands[$SLURM_ARRAY_TASK_ID]}"
+    echo "$command_to_run"
+    $command_to_run
+else
+  # If run manually, print the total number of combinations
+  echo "This script should be run as a SLURM array job."
+  echo "Use: sbatch --array=0-$((total_combinations-1))%999 $0"
+  echo "This will distribute $total_combinations jobs across N GPUs."
+fi
